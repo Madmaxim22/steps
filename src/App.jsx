33f0f5209ced0950a67cd0253c679030d59d7dc7
Максим Мухомедьяров <madmaxim@gmail.com>
2026-03-01 16:31:20 +0300

    init: steps project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [trainings, setTrainings] = useState('');
+
+  return (
+    <div className="container">
+      
+    </div>
+  )
+}
+
+export default App
